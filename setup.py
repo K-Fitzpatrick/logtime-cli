66a15a67c7c6b6e0b0ee3e2170c390293b45618e
K-Fitzpatick <k.fitzpatrick9@yahoo.com>
2017-05-26 01:15:26 -0500

    Create and include logtime_cli module

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='logtime-cli',
     version='0.3',
     scripts=['lt'],
+    packages=['logtime_cli'],
     description='CLI utility for logging the time you spend on things',
     author='Kyle Fitzpatrick',
     author_email='kyle.a.fitzpatrick@gmail.com',

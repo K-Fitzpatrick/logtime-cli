a2ea4117a986360e28c923ff0b0c46f133125657
K-Fitzpatick <k.fitzpatrick9@yahoo.com>
2017-06-10 22:55:46 -0500

    Replace all object names with pythonic names according to pylint
    Consistency with any convention is what's important,
    but I let pylint pick my convention :P

--- logtime_cli/logtime.py
+++ logtime_cli/logtime.py
@@ -7,145 +7,145 @@ from logtime_cli.logtime_config import GetOption
 
 OUTPUT_TIME_FORMAT = '%I:%M %p'
 
-def _formatEntry(previousTimeEntry, timeEntry, taskEntry, taskLength):
-    return "|\t" + str(previousTimeEntry) + "\t|\t" + str(timeEntry) + "\t|\t" + str(taskEntry) + "\t|\t" + str(taskLength) + "\t|\n"
+def _format_entry(previous_time_entry, time_entry, task_entry, task_length):
+    return "|\t" + str(previous_time_entry) + "\t|\t" + str(time_entry) + "\t|\t" + str(task_entry) + "\t|\t" + str(task_length) + "\t|\n"
 
 
-def _getFirstTimeEntry(entryLine):
-    match = re.findall("([0-1]*[0-9]:[0-6][0-9] [AP]M)", entryLine)
+def _get_first_time_entry(entry_line):
+    match = re.findall("([0-1]*[0-9]:[0-6][0-9] [AP]M)", entry_line)
     if match:
         return match[0]
     else:
         return None
 
 
-def _getSecondTimeEntry(entryLine):
-    match = re.findall("([0-1]*[0-9]:[0-6][0-9] [AP]M)", entryLine)
+def _get_second_time_entry(entry_line):
+    match = re.findall("([0-1]*[0-9]:[0-6][0-9] [AP]M)", entry_line)
     if match:
         return match[1]
     else:
         return None
 
 
-def _convertTimeOfDay(timeOfDay):
-    if timeOfDay == 'A' or timeOfDay == 'a':
+def _convert_time_of_day(time_of_day):
+    if time_of_day == 'A' or time_of_day == 'a':
         return "AM"
-    if timeOfDay == 'P' or timeOfDay == 'p':
+    if time_of_day == 'P' or time_of_day == 'p':
         return "PM"
     return None
 
 
-def _getTimeFromArgument(argTime):
-    military_time_match = re.search('^([0-1][0-9]|[2][0-3]):?([0-6][0-9])$', argTime)
-    standard_time_match = re.search('^([0][0-9]|[1][0-2]):?([0-6][0-9])([AaPp])$', argTime)
+def _get_time_from_argument(arg_time):
+    military_time_match = re.search('^([0-1][0-9]|[2][0-3]):?([0-6][0-9])$', arg_time)
+    standard_time_match = re.search('^([0][0-9]|[1][0-2]):?([0-6][0-9])([AaPp])$', arg_time)
 
     time_match = military_time_match or standard_time_match
     if not (time_match):
-        raise ValueError('"' + argTime + '" is not a valid time')
+        raise ValueError('"' + arg_time + '" is not a valid time')
 
     hours = time_match.group(1)
     minutes = time_match.group(2)
-    timeString = str(hours) + str(minutes)
+    time_string = str(hours) + str(minutes)
 
     if military_time_match:
-        return datetime.strptime(timeString, "%H%M").strftime(OUTPUT_TIME_FORMAT)
+        return datetime.strptime(time_string, "%H%M").strftime(OUTPUT_TIME_FORMAT)
     elif standard_time_match:
         meridiem = standard_time_match.group(3)
-        return datetime.strptime(timeString + _convertTimeOfDay(meridiem), "%I%M%p").strftime(OUTPUT_TIME_FORMAT)
+        return datetime.strptime(time_string + _convert_time_of_day(meridiem), "%I%M%p").strftime(OUTPUT_TIME_FORMAT)
 
 
-def _getLengthBetweenTimes(previousTimeEntry, timeEntry):
-    d1 = datetime.strptime(previousTimeEntry, OUTPUT_TIME_FORMAT)
-    d2 = datetime.strptime(timeEntry, OUTPUT_TIME_FORMAT)
+def _get_length_between_times(previous_time_entry, time_entry):
+    d1 = datetime.strptime(previous_time_entry, OUTPUT_TIME_FORMAT)
+    d2 = datetime.strptime(time_entry, OUTPUT_TIME_FORMAT)
     return (d2 - d1).total_seconds() / 3600
 
 
-def _updateLengthBetweenTimes(filePath):
-    f = open(filePath, "r")
-    readLines = f.readlines()
+def _update_length_between_times(file_path):
+    f = open(file_path, "r")
+    read_lines = f.readlines()
     f.close()
-    f = open(filePath, "w")
-    for currentLine in readLines:
-        firstTimeEntry = _getFirstTimeEntry(currentLine)
-        if firstTimeEntry:
-            secondTimeEntry = _getSecondTimeEntry(currentLine)
-            items = string.split(currentLine, "|")
-            f.write(_formatEntry(items[1].strip(), items[2].strip(), items[3].strip(), _getLengthBetweenTimes(firstTimeEntry, secondTimeEntry)))
+    f = open(file_path, "w")
+    for current_line in read_lines:
+        first_time_entry = _get_first_time_entry(current_line)
+        if first_time_entry:
+            second_time_entry = _get_second_time_entry(current_line)
+            items = string.split(current_line, "|")
+            f.write(_format_entry(items[1].strip(), items[2].strip(), items[3].strip(), _get_length_between_times(first_time_entry, second_time_entry)))
         else:
-            f.write(currentLine)
+            f.write(current_line)
     f.close()
 
 
-def _createNewLogFile(filePath):
-    f = open(filePath, "a+")
+def _create_new_log_file(file_path):
+    f = open(file_path, "a+")
     f.write("# Notes:\n\n\n")
     f.write("# Time log:\n\n")
-    f.write(_formatEntry("Start", "End", "Task", "Length"))
-    f.write(_formatEntry("---", "---", "---", "---"))
+    f.write(_format_entry("Start", "End", "Task", "Length"))
+    f.write(_format_entry("---", "---", "---", "---"))
     f.close()
 
 
-def _getLogFileDirectory():
-    logFileDirectory = GetOption('DEFAULT', 'logfile_directory')
-    if not os.path.exists(logFileDirectory):
-        os.mkdir(logFileDirectory)
-    return logFileDirectory
+def _get_log_file_directory():
+    log_file_directory = GetOption('DEFAULT', 'logfile_directory')
+    if not os.path.exists(log_file_directory):
+        os.mkdir(log_file_directory)
+    return log_file_directory
 
 
-def _getFilePathForDate(date):
+def _get_file_path_for_date(date):
     #Always reset path to this file
     if os.path.isdir(sys.path[0]):
         os.chdir(sys.path[0])
     else:
         os.chdir(os.path.dirname(sys.path[0]))
 
-    logFileDirectory = _getLogFileDirectory()
+    log_file_directory = _get_log_file_directory()
 
-    currentDate = date.isoformat()
-    filePath = os.path.join(logFileDirectory, currentDate + ".md")
+    current_date = date.isoformat()
+    file_path = os.path.join(log_file_directory, current_date + ".md")
 
-    return os.path.abspath(filePath)
+    return os.path.abspath(file_path)
 
 
-def _printLastLineToConsole(filePath):
-    f = open(filePath, "a+")
+def _print_last_line_to_console(file_path):
+    f = open(file_path, "a+")
     lines = f.readlines()
     print lines[len(lines)-1]
     f.close()
 
 
-def OpenLogfileForDate(dateToOpen, canCreate=False):
-    filePath = _getFilePathForDate(dateToOpen)
+def open_logfile_for_date(date_to_open, can_create=False):
+    file_path = _get_file_path_for_date(date_to_open)
 
-    if canCreate and not os.path.isfile(filePath):
-        _createNewLogFile(filePath)
+    if can_create and not os.path.isfile(file_path):
+        _create_new_log_file(file_path)
 
-    os.startfile(filePath)
+    os.startfile(file_path)
 
 
-def LogTime(taskEntry, start, end):
-    filePath = _getFilePathForDate(date.today())
+def log_time(task_entry, start, end):
+    file_path = _get_file_path_for_date(date.today())
 
-    if not os.path.isfile(filePath):
-        _createNewLogFile(filePath)
+    if not os.path.isfile(file_path):
+        _create_new_log_file(file_path)
 
-    _updateLengthBetweenTimes(filePath)
+    _update_length_between_times(file_path)
 
-    f = open(filePath, "a+")
+    f = open(file_path, "a+")
     lines = f.readlines()
-    lastTimeEntry = _getSecondTimeEntry(lines[len(lines)-1])
-    currentTimeEntry = datetime.today().time().strftime(OUTPUT_TIME_FORMAT)
+    last_time_entry = _get_second_time_entry(lines[len(lines)-1])
+    current_time_entry = datetime.today().time().strftime(OUTPUT_TIME_FORMAT)
 
     if start:
-        lastTimeEntry = _getTimeFromArgument(start)
+        last_time_entry = _get_time_from_argument(start)
     if end:
-        currentTimeEntry = _getTimeFromArgument(end)
+        current_time_entry = _get_time_from_argument(end)
 
-    if lastTimeEntry:
-        f.write(_formatEntry(lastTimeEntry, currentTimeEntry, taskEntry, _getLengthBetweenTimes(lastTimeEntry, currentTimeEntry)))
+    if last_time_entry:
+        f.write(_format_entry(last_time_entry, current_time_entry, task_entry, _get_length_between_times(last_time_entry, current_time_entry)))
     else:
-        startTime = GetOption("DEFAULT", "new_day_start_time")
-        f.write(_formatEntry(startTime, currentTimeEntry, taskEntry, _getLengthBetweenTimes(startTime, currentTimeEntry)))
+        start_time = GetOption("DEFAULT", "new_day_start_time")
+        f.write(_format_entry(start_time, current_time_entry, task_entry, _get_length_between_times(start_time, current_time_entry)))
     f.close()
 
-    _printLastLineToConsole(filePath)
+    _print_last_line_to_console(file_path)

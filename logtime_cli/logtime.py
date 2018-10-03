3fd262346b06e7ea8ae32833475833a82661f541
K-Fitzpatick <k.fitzpatrick9@yahoo.com>
2018-10-03 01:04:27 -0500

    Put back used code and make `-s` and `-e` work again

--- logtime_cli/logtime.py
+++ logtime_cli/logtime.py
@@ -4,12 +4,43 @@ Locate and manipulate logfiles.
 
 import os
 import sys
+import re
 from datetime import date, datetime
 from logtime_cli.logtime_config import get_option
 
 import logtime_cli.logfile_data as logfile_data
 
 OUTPUT_TIME_FORMAT = '%I:%M %p'
+
+def _convert_time_of_day(time_of_day):
+    if time_of_day == 'A' or time_of_day == 'a':
+        return "AM"
+    if time_of_day == 'P' or time_of_day == 'p':
+        return "PM"
+    return None
+
+
+def _get_time_from_argument(arg_time):
+    military_time_match = re.search('^([0-1][0-9]|[2][0-3]):?([0-6][0-9])$', arg_time)
+    standard_time_match = re.search('^([0][0-9]|[1][0-2]):?([0-6][0-9])([AaPp])$', arg_time)
+
+    time_match = military_time_match or standard_time_match
+    if not time_match:
+        raise ValueError('"' + arg_time + '" is not a valid time')
+
+    hours = time_match.group(1)
+    minutes = time_match.group(2)
+    time_string = str(hours) + str(minutes)
+
+    if military_time_match:
+        output_date = datetime.strptime(time_string, "%H%M")
+        return output_date.time()
+    elif standard_time_match:
+        meridiem = standard_time_match.group(3)
+        output_date = datetime.strptime(time_string + _convert_time_of_day(meridiem), "%I%M%p")
+        return output_date.time()
+    return None
+
 
 def _get_log_file_directory():
     log_file_directory = get_option('DEFAULT', 'logfile_directory')
@@ -99,10 +130,10 @@ def log_time(task_entry, start=None, end=None):
 
     end_time = datetime.today().time()
 
-    if start:
-        start_time = start
-    if end:
-        end_time = end
+    if start is not None:
+        start_time = _get_time_from_argument(start)
+    if end is not None:
+        end_time = _get_time_from_argument(end)
 
     if start_time is None:
         start_time_str = get_option("DEFAULT", "new_day_start_time")

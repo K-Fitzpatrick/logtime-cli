7378fdb3ab4f1dcab8acdf55265800334ba9f6d0
K-Fitzpatick <k.fitzpatrick9@yahoo.com>
2019-08-27 23:23:30 -0500

    Restrict installation to Python==2.*
    Any Python2 is probably fine, the point is to stop Python3 installations.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ## Unreleased
+### Fixed
+- Properly restrict installation to `Python==2.*`
 
 ## 1.4.2 - 2019-08-21
 ### Fixed
--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='logtime-cli',
     version='1.4.2',
+    python_requires='==2.*',
     scripts=['lt', 'logtime'],
     packages=find_packages(),
     description='CLI utility for logging the time you spend on things',
